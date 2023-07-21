1bc966707dd83fd1861fd418a8713ff120b6ef46
Jarod Kober <jarod.development@gmail.com>
2023-07-21 16:19:16 -0700

    Add & Delete in Movie Playlist Component * Created a slice for movies * Created reducers to add and delete a movie * Updated the MoviePlaylist component:     * Retrieve the movies array from the store and render them on the page     * Dispatch an action that adds a movie to the store using the new reducer     * Dispatch an action that removes a movie from the store using the new reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,19 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+const moviesSlice = createSlice({
+	name: 'movie',
+	initialState: [],
+	reducers: {
+		addMovie(state, action) {
+			state.push(action.payload);
+		},
+		removeMovie(state, action) {
+			const index = state.indexOf(action.payload);
+			state.splice(index, 1);
+		}
+	}
+});
+
 const songsSlice = createSlice({
 	name: 'song',
 	initialState: [],
@@ -16,9 +30,11 @@ const songsSlice = createSlice({
 
 const store = configureStore({
 	reducer: {
+		movies: moviesSlice.reducer,
 		songs: songsSlice.reducer
 	}
 });
 
 export { store };
+export const { addMovie, removeMovie } = moviesSlice.actions;
 export const { addSong, removeSong } = songsSlice.actions;
