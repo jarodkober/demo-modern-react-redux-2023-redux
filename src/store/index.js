365e26155688e2c1e7e30b2286d9169c34f14fce
Jarod Kober <jarod.development@gmail.com>
2023-07-21 15:56:22 -0700

    Add & Delete in Song Playlist Component
    * Created reducer to remove a song from the song store
    * Updated the SongPlaylist compnent:
        * Retrieve the songs array from the store and render them on the page
        * Dispatch an action that adds a song to the store (using an existing reducer)
        * Dispatch an action that removes a song from the store (using the new reducer)

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,8 @@ const songsSlice = createSlice({
 			state.push(action.payload);
 		},
 		removeSong(state, action) {
-			//
+			const index = state.indexOf(action.payload);
+			state.splice(index, 1);
 		}
 	}
 });
@@ -20,3 +21,4 @@ const store = configureStore({
 });
 
 export { store };
+export const { addSong, removeSong } = songsSlice.actions;
